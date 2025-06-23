ce85c61134735a405777dff1eb629ec2bbf09845
LAGM <luisiyi1997@gmail.com>
2025-06-22 20:53:17 -0700

    IndexControlador class added
    -added "IndexControlador" class, we set it as @Controller, here we will create different methods to manage the user´s requests like "get" and "post"
    -"/" is for manage the "home/inicio" request from the Frontend, inside we get all the contactos, we print the contactos, then we share all the contactos to the front end thankfully for "modelo.put("contactos", contactos)", and finally we redirect to the "index.html" view.

---
+++ src/main/java/gm/contactos/controlador/IndexControlador.java
@@ -0,0 +1,29 @@
+package gm.contactos.controlador;
+
+import gm.contactos.modelo.Contacto;
+import gm.contactos.servicio.ContactoServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class IndexControlador {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+
+    @Autowired
+    ContactoServicio contactoServicio;
+
+    @GetMapping("/")
+    public String inciciar(ModelMap modelo){
+        List<Contacto> contactos = contactoServicio.listarContactos();
+        contactos.forEach((contacto -> logger.info(contacto.toString())));
+        modelo.put("contactos", contactos);
+        return "index";
+    }
+}
