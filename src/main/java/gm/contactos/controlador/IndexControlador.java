ed5da7c49e1462ad138d3065c37c495ab6777f97
LAGM <luisiyi1997@gmail.com>
2025-06-28 12:35:09 -0700

    Editar with id parameter added in IndexControlador with front end
    -"editar.html" very same as "agregar.html" but we added the input with idContacto
    -"mostrarEditar" method in indexControlador, we receive an id and the modelmap, we share it with modelmap to the front end and redirecto to the "editar.html"
    -with all data sent by indexControlador here in the "editar.html" we used th:object="${contacto}" and enter each value in each input field with th:field="*{idContacto}", th:field="*{nombre}", th:field="*{celular}", and th:field="*{email}"

--- src/main/java/gm/contactos/controlador/IndexControlador.java
+++ src/main/java/gm/contactos/controlador/IndexControlador.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -39,5 +40,13 @@ public class IndexControlador {
         logger.info("Contacto a agregar: " + contacto);
         contactoServicio.guardarContacto(contacto);
         return "redirect:/";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarEitar(@PathVariable(value = "id") int idContacto, ModelMap modelo){
+        Contacto contacto = contactoServicio.buscarContactoPorId(idContacto);
+        logger.info("Contacto a editar (mostrar): " + contacto);
+        modelo.put("contacto", contacto);
+        return "editar";
     }
 }
